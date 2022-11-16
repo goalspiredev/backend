da37befe8c0dfae79c7272a97b74fb5b8db75ac1
encyk <martiiin225@email.cz>
2022-11-16 11:36:34 +0100

    feat: add settings model

---
+++ GoalspireBackend/Models/Settings.cs
@@ -0,0 +1,17 @@
+﻿namespace GoalspireBackend.Models;
+
+public enum GoalVisibility
+{
+    Private = 0, Public = 1
+}
+
+public class Settings
+{
+    public Guid UserId { get; set; }
+    public bool ReducedAnimations { get; set; }// = false;
+    public TimeSpan DefaultSnoozeDuration { get; set; }// = TimeSpan.FromMinutes(30);
+    public TimeOnly DailyNotificationTime { get; set; }// = new TimeOnly(7, 30); //07:30
+    public GoalVisibility DefaultGoalVisibility { get; set; }// = GoalVisibility.Private; // nebo tohle muze byt "bool DefaultGoalsArePublic = false"
+    public List<string> GoalTags { get; set; } = new List<string>();
+    public bool DisableEmailNotifications { get; set; }//= false
+}
